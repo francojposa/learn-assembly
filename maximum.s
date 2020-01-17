d5bdf9419aab4a89b0757827594ca1ec82051edf
francojposa <francojposa@gmail.com>
2020-01-16 20:41:59 -0600

    starting program to find max int in list

---
+++ maximum.s
@@ -0,0 +1,8 @@
+#PURPOSE:    This program finds the maximum number of a
+#            set of data items.
+#
+#VARIABLES:  The registers have the following uses:
+#
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
